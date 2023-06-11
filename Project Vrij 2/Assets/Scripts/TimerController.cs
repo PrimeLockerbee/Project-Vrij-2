d84aacee2bd63bde2bf33ed083de78208b6dcb7e
Bradley van Ewijk <32265034+PrimeLockerbee@users.noreply.github.com>
2023-06-11 22:18:06 +0200

    Time controller fix

--- Project Vrij 2/Assets/Scripts/TimerController.cs
+++ Project Vrij 2/Assets/Scripts/TimerController.cs
@@ -6,12 +6,15 @@ using TMPro;
 public class TimerController : MonoBehaviour
 {
     public float totalTime = 60f; // Total time in seconds for the effect
-    //public float vignetteIntensity = 0f; // Initial intensity of the vignette
+    public float vignetteIntensity = 0f; // Initial intensity of the vignette
     public float heartbeatVolume = 0.05f; // Initial volume of the heartbeat sound (starting value)
     public float textDisplayInterval = 5f; // Time interval between text displays
 
     public TextMeshProUGUI textDisplay; // Reference to the TextMeshProUGUI component
     public List<string> textsToDisplay; // Text options to be displayed
+
+    public GameObject functionTarget; // Reference to the GameObject to call the function on
+    public string functionName = "FunctionToCall"; // Name of the function to call on the target GameObject
 
     private float initialHeartbeatVolume; // Store the initial volume for reference
     private float timeElapsed = 0f; // Current time elapsed
@@ -20,6 +23,8 @@ public class TimerController : MonoBehaviour
 
     private AudioSource heartbeatAudio; // Reference to the audio source playing the heartbeat sound
     private GameObject parentObject; // Reference to the parent GameObject
+
+    private bool isTextDisplaying = true; // Flag to track if text is currently being displayed
 
     void Start()
     {
@@ -39,11 +44,26 @@ public class TimerController : MonoBehaviour
         float progress = Mathf.Clamp01(timeElapsed / totalTime);
 
         // Check if it's time to display text
-        if (textDisplayTimer >= textDisplayInterval)
+        if (isTextDisplaying && textDisplayTimer >= textDisplayInterval)
         {
             textDisplay.gameObject.SetActive(true);
             textDisplay.text = GetNextTextToDisplay();
-            StartCoroutine(HideTextAfterDelay(3f)); // Hide the text after a delay (3 seconds in this example)
+
+            if (currentTextIndex >= textsToDisplay.Count)
+            {
+                // All texts have been displayed
+                isTextDisplaying = false;
+
+                // Call the specified function on the target GameObject
+                if (functionTarget != null)
+                {
+                    functionTarget.SendMessage(functionName);
+                }
+            }
+            else
+            {
+                StartCoroutine(HideTextAfterDelay(3f)); // Hide the text after a delay (3 seconds in this example)
+            }
 
             textDisplayTimer = 0f; // Reset the text display timer
 
@@ -56,13 +76,13 @@ public class TimerController : MonoBehaviour
 
     private string GetNextTextToDisplay()
     {
-        if (textsToDisplay.Count == 0)
+        if (currentTextIndex >= textsToDisplay.Count)
         {
             return "";
         }
 
         string text = textsToDisplay[currentTextIndex];
-        currentTextIndex = (currentTextIndex + 1) % textsToDisplay.Count;
+        currentTextIndex++;
 
         return text;
     }
